3f764ab3162889120925bec398e30cdd235d93d1
ADITI PRIYA <aditipriya7237@gmail.com>
2023-04-13 01:26:49 +0530

    User Service Implemented

---
+++ payroll/src/main/java/com/aditi/payroll/service/Interface/UserService.java
@@ -0,0 +1,7 @@
+package com.aditi.payroll.service.Interface;
+
+import com.aditi.payroll.collections.User;
+
+public interface UserService {
+    User save(User user);
+}
---
+++ payroll/src/main/java/com/aditi/payroll/service/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.aditi.payroll.service;
+
+import com.aditi.payroll.collections.User;
+import com.aditi.payroll.repository.UserRepository;
+import com.aditi.payroll.service.Interface.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
