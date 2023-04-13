6cbf4f5b9c13ba2cef7d72a5970a3ab03454b7ca
ADITI PRIYA <aditipriya7237@gmail.com>
2023-04-14 01:16:04 +0530

    Getmapping controller created

--- payroll/src/main/java/com/aditi/payroll/controller/UserController.java
+++ payroll/src/main/java/com/aditi/payroll/controller/UserController.java
@@ -5,10 +5,9 @@ import com.aditi.payroll.service.Interface.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -20,6 +19,11 @@ public class UserController {
     public ResponseEntity<User> saveUser(@RequestBody User user) {
         user.setId(System.currentTimeMillis());
         return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUser() {
+        return new ResponseEntity<>(userService.getAllUser(), HttpStatus.CONFLICT);
     }
 }
 
--- payroll/src/main/java/com/aditi/payroll/service/Interface/UserService.java
+++ payroll/src/main/java/com/aditi/payroll/service/Interface/UserService.java
@@ -2,6 +2,10 @@ package com.aditi.payroll.service.Interface;
 
 import com.aditi.payroll.collections.User;
 
+import java.util.List;
+
 public interface UserService {
     User save(User user);
+
+    List<User> getAllUser();
 }
