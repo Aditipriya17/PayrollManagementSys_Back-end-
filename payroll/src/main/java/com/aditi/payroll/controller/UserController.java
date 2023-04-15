41b862cb1005c66943bf3f965205a277097c066f
ifshaul.haque <ifshaul.haque@gmail.com>
2023-04-16 02:33:45 +0530

    add cors permission and encoded password

--- payroll/src/main/java/com/aditi/payroll/controller/UserController.java
+++ payroll/src/main/java/com/aditi/payroll/controller/UserController.java
@@ -2,22 +2,27 @@ package com.aditi.payroll.controller;
 
 import com.aditi.payroll.collections.User;
 import com.aditi.payroll.service.Interface.UserService;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> saveUser(@RequestBody User user) {
+    public ResponseEntity<User> saveUser(@RequestBody User user) throws NoSuchAlgorithmException {
         user.setId(System.currentTimeMillis());
+        user.setPassword(sha256(user.getPassword()));
         return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
     }
 
@@ -29,6 +34,12 @@ public class UserController {
     @GetMapping("/{email}")
     public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
         return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.FOUND);
+    }
+
+    public String sha256(String input) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = messageDigest.digest(input.getBytes());
+        return Hex.encodeHexString(hash);
     }
 }
 
